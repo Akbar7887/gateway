7b1097ecf2ab3e95d35161509dc1e120026c1968
Akbar <akbar358888@gmail.com>
2022-09-25 12:07:44 +0500

    Initial commit

---
+++ src/main/java/com/sharqserver/gateway/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.sharqserver.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+
+//@EnableWebFluxSecurity
+//@EnableReactiveMethodSecurity
+@Configuration
+public class CorsConfig {
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource()  {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:8087"));
+        config.setAllowedMethods(Collections.singletonList("GET"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
